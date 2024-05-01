a86841c42ac1ecb4af59b18b5401aa6853302057
MuriloFlores <muriloaflores@gmail.com>
2024-04-30 22:26:09 -0300

    ~

--- app/server.go
+++ app/server.go
@@ -25,13 +25,14 @@ func handleMessage(conn net.Conn, message string) {
 }
 
 func handleConnection() net.Conn {
-	for {
 
-		l, err := net.Listen("tcp", "0.0.0.0:6379")
-		if err != nil {
-			fmt.Println("Failed to bind to port 6379")
-			os.Exit(1)
-		}
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	
+	for {
 
 		conn, err := l.Accept()
 		if err != nil {
