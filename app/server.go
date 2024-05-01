33a863225a28b4f5c11f6e16e3073bb02a8c3c70
MuriloFlores <muriloaflores@gmail.com>
2024-04-30 21:32:56 -0300

    test

--- app/server.go
+++ app/server.go
@@ -24,21 +24,28 @@ func handleMessage(conn net.Conn, message string) {
 	}
 }
 
+func handleConnection(port string) net.Conn {
+	for {
+
+		l, err := net.Listen("tcp", port)
+		if err != nil {
+			fmt.Println("Failed to bind to port 6379")
+			os.Exit(1)
+		}
+
+		conn, err := l.Accept()
+		if err == nil {
+			fmt.Println("Error accepting connection")
+			os.Exit(1)
+		}
+
+		handleMessage(conn, "+PONG\r\n")
+	}
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	handleConnection("0.0.0.0:6379")
 
-	//enviando a mensagem
-	//seding message
-	handleMessage(conn, "+PONG\r\n")
 }
